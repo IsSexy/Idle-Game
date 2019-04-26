4cf7981bcffefa276fdb34ab9495690105959ce1
Brady W <i.am.brady.ward@gmail.com>
2019-04-26 18:25:40 -0400

    The beginning
    First push with extremely basic HTML and pretty much no JS.

---
+++ main.js
@@ -0,0 +1,4 @@
+//HTML Selectors
+const playerIcon = document.querySelector('#playerIcon');
+const enemyIcon = document.querySelector('#enemyIcon');
+const attackBtn = document.querySelector('#attackBtn');
