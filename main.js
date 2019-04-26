b20b511c98d5b86400c8e30e7ece51d83ac83194
Brady W <i.am.brady.ward@gmail.com>
2019-04-26 18:54:46 -0400

    Update main.js
    Quick update

--- main.js
+++ main.js
@@ -2,3 +2,22 @@
 const playerIcon = document.querySelector('#playerIcon');
 const enemyIcon = document.querySelector('#enemyIcon');
 const attackBtn = document.querySelector('#attackBtn');
+
+function IdleGame()
+{
+	this.player = 
+	{
+		name: 'Player One',
+		maxHp: 50,
+		currentHp: 50,
+		damage: 5
+	};
+
+	this.enemies = 
+	[
+		{ name: 'Enemy1', image: '', maxHp: 50 },
+		{ name: 'Enemy2', image: '', maxHp: 25 },
+		{ name: 'Enemy3', image: '', maxHp: 15 },
+		{ name: 'Enemy4', image: '', maxHp: 35 }
+	]
+}
