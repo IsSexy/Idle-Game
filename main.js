49bdd63c9c7253bb41e457034d086c284a19165f
Brady W <i.am.brady.ward@gmail.com>
2019-05-01 20:14:53 -0400

    Update main.js

--- main.js
+++ main.js
@@ -2,6 +2,12 @@
 const playerIcon = document.querySelector('#playerIcon');
 const enemyIcon = document.querySelector('#enemyIcon');
 const attackBtn = document.querySelector('#attackBtn');
+var currentEnemy;
+
+function randomInt(max)
+{
+	return Math.floor(Math.random() * Math.floor(max));
+}
 
 function attack()
 {
@@ -17,35 +23,58 @@ function IdleGame()
 		name: 'Player One',
 		maxHp: 50,
 		currentHp: 50,
-		damage: 5
+		damage: 5,
+		level: 1,
+		exp: 0
 	};
 
 	this.enemies = 
 	[
-		{ name: 'Enemy1', image: '', maxHp: 50 },
-		{ name: 'Enemy2', image: '', maxHp: 25 },
-		{ name: 'Enemy3', image: '', maxHp: 15 },
-		{ name: 'Enemy4', image: '', maxHp: 35 }
-	]
+		{ name: 'Enemy1', image: '', maxHp: 50, currentHp: 50, exp: 50 },
+		{ name: 'Enemy2', image: '', maxHp: 25, currentHp: 25, exp: 25 },
+		{ name: 'Enemy3', image: '', maxHp: 15, currentHp: 15, exp: 15 },
+		{ name: 'Enemy4', image: '', maxHp: 35, currentHp: 35, exp: 35 }
+	];
 
 	this.startGame();
 }
 
 IdleGame.prototype.startGame = function()
 {
-	playerIcon.textContent = this.player.maxHp;
-	enemyIcon.textContent = this.enemies[0].maxHp;
+	this.newEnemy();
+	this.refreshFight();
 }
 
 IdleGame.prototype.refreshFight = function()
 {
 	playerIcon.textContent = this.player.maxHp;
-	enemyIcon.textContent = this.enemies[0].maxHp;
+	enemyIcon.textContent = currentEnemy.maxHp;
+}
+
+IdleGame.prototype.newEnemy = function()
+{
+	let num = randomInt(this.enemies.length);
+	Object.assign(currentEnemy, this.enemies[num]);
+	/*
+	currentEnemy.maxHp = this.enemies[num].maxHp;
+	currentEnemy.name = this.enemies[num].name;
+	*/
 }
 
 IdleGame.prototype.attackEnemy = function()
 {
-	this.enemies[0].maxHp -= this.player.damage;
+	currentEnemy.maxHp -= this.player.damage;
+	if(currentEnemy.maxHp <= 0)
+	{
+		this.player.exp += currentEnemy.exp;
+		if(this.player.exp % 100 === 0)
+		{
+			this.player.damage += 1;
+			this.player.maxHp += 5;
+			this.player.currentHp = this.player.maxHp;
+		}
+		this.newEnemy();
+	}
 	this.refreshFight();
 	console.log('Attacked');
 }
